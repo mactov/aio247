117a0e5ab2e74b1eab14d6da2e83b62ea38b63d1
mactov <ch.leborgne@gmail.com>
2015-04-20 23:21:52 +0200

    visibilité des boutons

--- static/js/aio.js
+++ static/js/aio.js
@@ -1,11 +1,23 @@
-var aioApp = angular.module('aio', []);
-
-aioApp.controller('welcomeController', function($scope) {
+angular.module('aio', [])
+.controller('aioController', function($scope) {
     $scope.userRole = 'unknown';
     $scope.updateRoleToCustomer = function() {
-    $scope.userRole = 'customer';
+        $scope.userRole = 'customer';
     };
 	$scope.updateRoleToProvider = function() {
-    $scope.userRole = 'provider';
+        $scope.userRole = 'provider';
     };
-}
+    $scope.showWelcome = function() {
+        return ($scope.userRole == 'unknown');
+    };
+    $scope.userHasAccount = null;
+    $scope.updateUserHasAccount = function() {
+        $scope.userHasAccount = true;
+    };
+	$scope.updateUserHasNoAccount = function() {
+        $scope.userHasAccount = false;
+    };
+    $scope.showAccount = function() {
+        return ($scope.userHasAccount == null && $scope.userRole != 'unknown');
+    };
+});
